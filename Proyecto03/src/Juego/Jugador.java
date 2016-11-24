5a1c16f7765ac9a34670976020fe267d8a3d48fd
AndreuCastello <AndreuCastello9lol@gmail.com>
2016-11-24 23:17:22 +0100

    Estos son los codigos de El cuadro para los datos del jugador y que datos se piden

--- Proyecto03/src/Juego/Jugador.java
+++ Proyecto03/src/Juego/Jugador.java
@@ -3,9 +3,9 @@ package Juego;
 public class Jugador {
 	private String nombre;
 	private String apellidos;
-	private String apellido1;
-	private String apellido2;
-	private int edad;
+	private String PrimerApellido;
+	private String SegundoApellido;
+	private int Edad;
 	private int puntos;
 	private int id;
 	
@@ -28,11 +28,11 @@ public class Jugador {
 	}
 	
 	public int getEdad() {
-		return edad;
+		return Edad;
 	}
 	
-	public void setEdad(int edad) {
-		this.edad = edad;
+	public void setEdad(int Edad) {
+		this.Edad = Edad;
 	}
 	
 	public int getPuntos() {
@@ -43,20 +43,20 @@ public class Jugador {
 		this.puntos = puntos;
 	}
 	
-	public String getApellido1() {
-		return apellido1;
+	public String getPrimerApellido() {
+		return PrimerApellido;
 	}
 	
-	public void setApellido1(String apellido1) {
-		this.apellido1 = apellido1;
+	public void setPrimerApellido(String PrimerApellido) {
+		this.PrimerApellido = PrimerApellido;
 	}
 	
-	public String getApellido2() {
-		return apellido2;
+	public String getSegundoApellido() {
+		return SegundoApellido;
 	}
 	
-	public void setApellido2(String apellido2) {
-		this.apellido2 = apellido2;
+	public void setSegundoApellido(String SegundoApellido) {
+		this.SegundoApellido = SegundoApellido;
 	}
 	
 	public int getId() {
@@ -71,12 +71,31 @@ public class Jugador {
 		
 	}
 	
-	
-	
-	public String toString() {
-		return this.nombre+" "+this.apellido1+" "+this.apellido2+" "+this.edad;
+	//Metodo para comprobar si una cadena es un numero
+	//Devuelve True si es un numero
+	public boolean isNumeric(String cadena){
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
+		
 	}
 	
+	//Metodo para comprobar si una cadena es un conjunto de espacio.
+	public boolean sonEspacios(String cad)
+	{
+	for(int i =0; i<cad.length(); i++)
+	if(cad.charAt(i) != ' ')
+	return false;
 	
+	return true;
+	}
+	
+	public String toString() {
+		return this.nombre+" "+this.PrimerApellido+" "+this.SegundoApellido+" de "+this.Edad+" años";
+	}
+
 	
 }
